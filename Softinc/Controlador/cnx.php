d8aeadcacedf4e5c8fcfdd77d6e3bea67a69d221
wilder5912 <wilder.5912@gmail.com>
2013-09-26 00:37:51 -0430

    interfaz con imagenes y conexion a db

---
+++ Softinc/Controlador/cnx.php
@@ -0,0 +1,8 @@
+<?php
+$host="localhost"; //hosting
+$user="postgres";  //usuario de postgres por defecto
+$pw="postgres123"; //contraseña
+$port = 5432;      //puerto de postgres
+$bd="tisPrueba";      //base de datos a conectar
+$entrada="host=$host port=$port dbname=$bd user=$user password=$pw";
+?>
