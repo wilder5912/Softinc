ac241e0cd23afe641d47a540502a43684d34a0d5
wilder5912 <wilder.5912@gmail.com>
2013-10-11 22:08:36 -0430

    borrando

--- Softinc/Controlador/cnx.php
+++
@@ -1,9 +0,0 @@
-<?php
-global $entrada;
-$host="localhost"; //hosting
-$user="postgres";  //usuario de postgres por defecto
-$pw="postgres"; //contraseña
-$port = 5432;      //puerto de postgres
-$bd="olimpiada";      //base de datos a conectar
-$entrada = pg_connect( "host=$host port=$port dbname=$bd user=$user password=$pw");
-?>
