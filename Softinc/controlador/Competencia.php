578ad3de347d792f939eb7180f89c3f906e336d6
aizaChoke <prodigy_0-07@hotmail.com>
2013-11-04 00:29:22 -0430

    limpiando

--- Softinc/controlador/Competencia.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(isset($_POST['competencia'])){
-$competencia=$_POST['competencia'];
-
-require '../modelo/Consulta.php';
-$consulta=new Consulta();
-echo $consulta->generarProblemasCompetencia($competencia);
-}
-?>
