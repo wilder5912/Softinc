68dd6b289f71b09d8a25f4f2de650ada35a34516
naira88re <naira88re@gmail.com>
2013-11-05 23:28:10 -0400

    Agregando estilos

---
+++ Softinc/vista/aplicacion/cabecera.php
@@ -0,0 +1,45 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Juez virtual</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="../css/estilos_aplicacion.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body>
+    
+<div id="wrapper">
+	<div id="logo">
+		<h1><a href="#">Bienvenido al Juez Virtual SoftInc</a></h1>
+		<p><em> Here you will find hundreds of problems.</em></p>
+	</div>
+	<hr />
+	<!-- end #logo -->
+	<div id="header">
+		<div id="menu">
+			<ul>
+				<li><a href="#" class="first">Home</a></li>
+				<li class="current_page_item"><a href="#">Olimpiadas</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Perfil</a></li>
+				<li><a href="#">Cerrar Sesion</a></li>
+			</ul>
+		</div>
+		<!-- end #menu -->
+		<div id="search">
+			<form method="get" action="">
+				<fieldset>
+				<input type="text" name="s" id="search-text" size="15" />
+				<input type="submit" id="search-submit" value="GO" />
+				</fieldset>
+			</form>
+		</div>
+		<!-- end #search -->
+	</div>
+	<!-- end #header -->
+<table>
+    <tr class="texto-tipo-usuario">
+        <td>Olimpista: </td>
+        <td>Naira Romero Echeverria</td>
+    </tr>
+</table>
---
+++ Softinc/vista/aplicacion/index.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once 'cabecera.php';
+?>
+
+
+<div>
+   <table class="tabla-principal">
+       <tr>
+           <td class="contenedor-barra-izquierda">
+                <ul class="sidebar">
+                    <li><a href="#"> Problemas</a></li>
+                    <li class="current_page_item"><a href="#">Presentar Soluci&oacute;n</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Perfil</a></li>
+                    <li><a href="#">Cerrar Sesion</a></li>
+                </ul>
+           </td>
+           <td><iframe src="https://www.google.com.bo" width="100%" height="100%"></iframe></td>
+       </tr>
+   </table>
+</div>
+
+<?php
+require_once 'pie.php';
+
+?>
